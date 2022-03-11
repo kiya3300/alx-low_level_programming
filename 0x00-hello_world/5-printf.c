a1e45d46ec513becb8e7199d74b439685c792fe8
kiya3300 <mekidesyitagesu18@gmail.com>
2022-03-11 03:34:20 +0300

    leave me alone

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+.#include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- *
- * Return: 0 if success.
- */
+* main - Entry point
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
