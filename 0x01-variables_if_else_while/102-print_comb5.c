ef51b2ebc82ca9fed3d1a9b97d0bfcfdda7fd143
kiya3300 <mekidesyitagesu18@gmail.com>
2022-03-12 02:17:36 +0300

    The last one

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,30 @@
 
 int main(void)
 {
-  int i;
-  int j;
+        int i;
+        int j;
 
-  for (i = 0; i < 100; i++)
-  {
-    for (j = 0; j < 100; j++)
-    {
-      if (j > i)
-      {
-        putchar(i / 10 + '0');
-        putchar(i % 10 + '0');
-        putchar(' ');
-        putchar(j / 10 + '0');
-        putchar(j % 10 + '0');
-
-        if (i * 100 + j != 9899)
+        for (i = 0; i < 100; i++)
         {
-          putchar(',');
-          putchar(' ');
-        }
-      }
-    }
-  }
-  putchar('\n');
+                for (j = 0; j < 100; j++)
+                {
+                        if (j > i)
+                        {
+                                putchar(i / 10 + '0');
+                                putchar(i % 10 + '0');
+                                putchar(' ');
+                                putchar(j / 10 + '0');
+                                putchar(j % 10 + '0');
 
-  return (0);
+                                if (i * 100 + j != 9899)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+
+        return (0);
 }
