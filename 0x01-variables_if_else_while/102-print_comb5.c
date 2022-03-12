bf9010cdf36efa027d32ed2492d7ff55dd3b6389
kiya3300 <mekidesyitagesu18@gmail.com>
2022-03-12 03:37:47 +0300

    wth alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,24 +6,24 @@
  */
 int main(void)
 {
-int i = 48;
-int j = 48;
-int k = 48;
-int l = 48;
-for (i = 48; i <= 57; i++)
+int i;
+int j;
+int k;
+int l;
+for (i = 0; i < 100; i++)
 {
-for (j = 48 ; j <= 57; j++)
+for (j = 0; j < 100; j++)
 {
-for (k = 48 ; k <= 57; k++)
+for (k = 0; k < 100; k++)
 {
-for (l = 49 ; l <= 57; l++)
+for (l = 0; l < 100; l++)
 {
 putchar(i);
 putchar(j);
 putchar(' ');
 putchar(k);
 putchar(l);
-if (i < 57 || j < 57 || k < 57 || l < 57)
+if (i < 10  j < 10  k < 10 || l < 10)
 {
 putchar(',');
 putchar(' ');
