085aa8f8dcc720fb5bcf0470293bc464e9e17a81
kiya3300 <mekidesyitages.com>
2022-03-15 15:53:20 -0700

    askf wuferlbgu

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
 void times_table(void)
 {
 int i, j, k;
+
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
@@ -18,7 +19,7 @@ _putchar(k + '0');
 else if (k <= 9)
 {
 _putchar(',');
-_putchar(' '); 
+_putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
@@ -32,4 +33,4 @@ _putchar(k % 10 + '0');
 }
 _putchar('\n');
 }
-} 
+}
