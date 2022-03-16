9b6e06b7b708aaaf2930d23bc14e14e3bba17ce0
kiya3300 <mekidesyitages.com>
2022-03-15 17:14:02 -0700

    abezahew kemer wet regetk

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
-#define LARGEST 10000000000
 /**
- * main - main block
- * Description: Find and print the first 98 fib numbers starting with 1 and 2.
- * Numbers should be coma and space separated.
- * Return: 0
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-  unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
-  unsigned long int hold1, hold2, hold3;
-  int count;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-  printf("%lu, %lu, ", bk1, bk2);
-  for (count = 2; count < 98; count++)
-  {
-    if (bk1 + bk2 > LARGEST  fr2 > 0  fr1 > 0)
-    {
-      hold1 = (bk1 + bk2) / LARGEST;
-      hold2 = (bk1 + bk2) % LARGEST;
-      hold3 = fr1 + fr2 + hold1;
-      fr1 = fr2, fr2 = hold3;
-      bk1 = bk2, bk2 = hold2;
-      printf("%lu%010lu", fr2, bk2);
-    }
-    else
-    {
-      hold2 = bk1 + bk2;
-      bk1 = bk2, bk2 = hold2;
-      printf("%lu", bk2);
-    }
-    if (count != 97)
-      printf(", ");
-  }
-  printf("\n");
-  return (0);
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
+	return (0);
 }
