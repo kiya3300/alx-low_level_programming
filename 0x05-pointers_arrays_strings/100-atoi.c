28fc702cfdabf1a850fd9ac8721b5ad1d04fce97
kiya3300 <99829666+kiya3300@users.noreply.github.com>
2022-03-22 00:44:02 +0300

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - print reverse characters.
+ * @s: Array
+ *
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] < '0' || s[i] > '9') && (s[i] == '-'))
+		{
+			sign = sign * -1;
+		}
+		else if ((s[i] >= '0' && s[i] <= '9'))
+		{
+			num = num * 10 + (s[i] - '0');
+			if ((s[i + 1] < '0' || s[i + 1] > '9'))
+			{
+				break;
+			}
+		}
+		i++;
+	}
+	return (num * sign);
+}
