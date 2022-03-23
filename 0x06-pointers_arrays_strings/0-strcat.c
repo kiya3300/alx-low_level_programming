10e1e91f3efba8910ff3b682a191df7d7e44c3f0
kiya3300 <99829666+kiya3300@users.noreply.github.com>
2022-03-23 22:53:05 +0300

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcat -  function that concatenates two strings.
+ * @dest: s1
+ * @src: s2
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int i = 0, c = 0, n = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+	while (n <= c)
+	{
+		dest[i] = src[n];
+		n++;
+		i++;
+	}
+	return (dest);
+}
