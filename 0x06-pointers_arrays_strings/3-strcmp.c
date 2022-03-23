f014596fa0e03231b0fcdfde8aa8bb8a06cef5f5
kiya3300 <99829666+kiya3300@users.noreply.github.com>
2022-03-23 23:04:31 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that copies a string.
+ *
+ * @s1: s1
+ * @s2: s2
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
