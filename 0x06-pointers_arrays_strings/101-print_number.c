86c392694e227c1e8c6994f3c857bbb3c70c15ac
kiya3300 <99829666+kiya3300@users.noreply.github.com>
2022-03-23 23:48:17 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,18 @@
  */
 void print_number(int n)
 {
-	unsigned int num;
+  unsigned int num;
 /*check if number is negative*/
-	num = n;
-	if (n < 0)
-	{
-		_putchar(45);
-		num = -n;
-	}
+  num = n;
+  if (n < 0)
+  {
+    _putchar(45);
+    num = -n;
+  }
 /* print number by recursion*/
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+  if (num / 10)
+  {
+    print_number(num / 10);
+  }
+  _putchar((num % 10) + '0');
 }
