b93a4a29bee0563a1e942af820b1450e6bf10e91
kiya3300 <99829666+kiya3300@users.noreply.github.com>
2022-03-28 23:08:33 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -12,4 +13,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
