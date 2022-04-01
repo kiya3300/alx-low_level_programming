612cb0210fab3ed61153f8d6a0d20e1af0e328e2
kiya3300 <99829666+kiya3300@users.noreply.github.com>
2022-04-01 22:08:30 +0300

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,3 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+#endif
